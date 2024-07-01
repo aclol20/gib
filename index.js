22fd1b40919840c90541a8b330ed13db8c556abb
AMAZING-MAROKE <marokemaroke2020@gmail.com>
2024-07-01 22:52:05 +0100

    alll

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = 5000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 // Routes
 app.get('/', (req, res) => {
